5a9eea855d9f9be7bbf41bdb9b2a009355f7efb3
Wesley Lane Wilson <ww2942a@student.american.edu>
2023-11-10 21:13:01 -0500

    finished initial models

--- r/stop_outcomes_script.R
+++ r/stop_outcomes_script.R
@@ -15,16 +15,12 @@ find_best_k <- function(k, x_variables, y_variable) {
   yhat <- knn(train = xtrain, test = xtest, cl = ytrain, k = k)
   
   # extract performance metrics 
-  err_rate = mean(yhat != ytest) #Error rate --> the prediction is not correct
-  true_positive_rate = sum(yhat == 1 & ytest == 1) / sum(ytest == 1) # TP/P
-  false_positive_rate = sum(yhat == 1 & ytest == 0) / sum(ytest == 0) #FP/N
-  
+  accuracy_rate = 1 - mean(yhat != ytest) #Accuracy rate rate --> the prediction is not correct
+
   #results
   results <- tibble(
     k = k, 
-    err_rate = err_rate, 
-    true_positive_rate = true_positive_rate, 
-    false_positive_rate = false_positive_rate
+    accuracy_rate = accuracy_rate
   )
   return(results)
 }
@@ -52,7 +48,7 @@ stop_data <- clean_names(stop_data)
 
 #based on reading the documentation of what might be potentially useful variables.
 stop_data <- stop_data %>%
-  select(
+  dplyr::select(
     #outcomes of the stop
     stop_type,  arrest_charges, tickets_issued,ticket_count, warnings_issued, warning_count,
     # information about the stop
@@ -82,7 +78,7 @@ stop_data <- stop_data %>%
     is.na(primary_stop_reason) ~ "Unknown",
     TRUE ~ primary_stop_reason)) %>% 
   # replace na values for ticket counts
-  mutate(across(c(ticket_count, warning_count), ~ replace_na(0))) %>% 
+  mutate(across(c(ticket_count, warning_count), ~ replace_na(., 0))) %>% 
   # create variables for time of day, day of the week, etcetera. 
   mutate(datetime = ymd_hms(datetime)) %>% 
   mutate(day_of_week = wday(datetime, label = TRUE)) %>% 
@@ -92,40 +88,121 @@ stop_data <- stop_data %>%
     hour_of_day >= 12 & hour_of_day < 17 ~ "Afternoon",
     hour_of_day >= 17 & hour_of_day < 20 ~ "Evening",
     TRUE ~ "Night"
-  ), .keep = "unused") %>% 
-  # these are binary columns that I will turn to factors. 
-  mutate(across(c(person_searched, property_searched, traffic_involved), ~ as.factor(.x))) %>% 
+  )) %>% 
   # some stop durations are clearly data entry errors (with stops spanning several days!) I will remove them.
   filter(stop_duration_mins < 60 * 8) %>% 
   mutate(age = as.numeric(age)) %>% 
-  # select the final variables 
-  select(! c(stop_type, datetime))
+  # select the final variables-->drop na values because modeling wont work with them.  
+  filter(stop_duration_mins > 0) %>% 
+  dplyr::select(-stop_type, -datetime) %>% 
+  drop_na()
+
 
 # export in case my colleagues want to use the same data.
 write_rds(x = stop_data, file = "data/final/wes_cleaned_stop_data.rds")
 
-
-
 # Exploratory Analysis ----------------------------------------------------
 stop_data %>% 
   group_by(stop_outcome) %>% 
-  summarise(mean = mean(stop_duration_mins, na.rm = T), 
-            sd = sd(stop_duration_mins), 
+  summarise(mean_duration = mean(stop_duration_mins, na.rm = T), 
+            sd_duration = sd(stop_duration_mins), 
+            mean_age = mean(age, na.rm = TRUE), 
+            sd_age = sd(age, na.rm = TRUE),
             n = n()) %>% 
   ungroup() %>% 
   mutate(prop = n / sum(n))
 
+# looking at the SD of the age and duration, I think qda or knn will be better. 
+# create a graph to analyze how the x variables are distributed 
+ggplot(data = slice_sample(stop_data, n = 1000), mapping = aes(y = stop_duration_mins, x = age, color = stop_outcome), alpha = .5) +
+  geom_jitter()
 
+# well the x variables are not normally distributed. 
+qqplot(x = stop_data$stop_duration_mins, stop_data$age)
 # Models ------------------------------------------------------------------
+## KNN ---------------------------------------------------------------------
+
+### KNN -- Non-tuned -- with model matrix approach ------------------------------------------------------
+# I have to create a model matrix given how many categorical variables there are. 
+# this is the data that will work for knn. I am going to put the others in a separate dataframe to build a model matrix. 
+knn_data <- stop_data %>% 
+  dplyr::select(stop_outcome, stop_duration_mins, person_searched, property_searched, traffic_involved, age, hour_of_day, day_of_week)
+
+matrix_xs <- stop_data[, setdiff(names(stop_data), names(knn_data))] %>% 
+  dplyr::select(!warning_count) %>% 
+  drop_na()
+
+#now create the model matrix. 
+new_data <- model.matrix(lm(ticket_count ~ . -1, data = matrix_xs)) 
+
+#put it all together
+knn_data <- cbind(knn_data, new_data) 
+
+# given how big the data set is, a 70/30 split seems appropriate. 
+model_data <- split_train_and_test_data(split_pct = .7, data = knn_data, seed = 123)
 
 
-# KNN ---------------------------------------------------------------------
+x_variables <- setdiff(names(knn_data), c("stop_outcome", "day_of_week"))
+outcome_variable <- "stop_outcome"
+
+xtrain <- model_data$train[, x_variables] 
+x_test <- model_data$test[, x_variables]
+y_train <- model_data$train[[outcome_variable]]
+y_test <- model_data$test[[outcome_variable]]
+
+yhat <- knn(train = xtrain, test = x_test, cl = y_train, k = 1)
+
+knn_model_matrix <- 1 - mean(yhat != y_test)
+knn_model_matrix
 
 
+### KNN -- Non-tuned Without model matrix approach --------------------------------------------------------
+knn_data <- stop_data %>% 
+  mutate(male = if_else(gender == "Male", 1, 0)) %>% 
+  mutate(white_person = if_else(ethnicity == "White", 1, 0))
+  
+
+model_data <- split_train_and_test_data(split_pct = .7, data = knn_data, seed = 123)
+
+x_variables <- c("stop_duration_mins","person_searched", "property_searched", "traffic_involved", "age", "male", "white_person")
+x_variables
+
+xtrain <- model_data$train[, x_variables] 
+x_test <- model_data$test[, x_variables]
+y_train <- model_data$train[[outcome_variable]]
+y_test <- model_data$test[[outcome_variable]]
+
+yhat <- knn(train = xtrain, test = x_test, cl = y_train, k = 1)
+
+knn_regular <- 1 - mean(yhat != y_test)
+knn_regular
+
+### KNN -- Tuned Without model matrix approach --------------------------------------------------------
+metrics <- map_dfr(1:50, ~ find_best_k(k = ., x_variables = x_variables, y_variable = "stop_outcome"))
+
+tuned_knn_no_matrix <- metrics[which.max(metrics$accuracy_rate), ]
+tuned_knn_no_matrix
 ## LDA and QDA -------------------------------------------------------------
 
+### LDA Model ---------------------------------------------------------------
 
+# wow, lda does the matrix automatically...
+lqda_data <- stop_data %>% 
+  dplyr::select(! c(ticket_count, warning_count))
 
+lda_all <- lda(data = lqda_data, stop_outcome ~ ., CV = TRUE)
+lda_all
+
+lda_accuracy <- mean(lda_all$class == stop_data$stop_outcome)
+
+lda_accuracy
+
+### QDA Model ---------------------------------------------------------------
+qda_all <- lda(data = lqda_data, stop_outcome ~ ., CV = TRUE)
+
+qda_accuracy <- mean(qda_all$class == stop_data$stop_outcome)
+
+qda_accuracy
 # Cross-Validation --------------------------------------------------------
 
 
